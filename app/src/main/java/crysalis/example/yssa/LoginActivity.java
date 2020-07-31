109babb66714c35c8e771d5f6565784d7581315e
RClement5700 <Rclement5700@gmail.com>
2020-07-31 04:33:11 -0400

    Update: changed from traditional to viewBinding method

--- app/src/main/java/crysalis/example/yssa/LoginActivity.java
+++ app/src/main/java/crysalis/example/yssa/LoginActivity.java
@@ -2,7 +2,10 @@ package crysalis.example.yssa;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import crysalis.example.yssa.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,8 +13,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        
+        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
     }
 }
