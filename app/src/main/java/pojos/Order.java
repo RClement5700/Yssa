2a600f1131a93987d82ffeef5a643c3f2cd1d07c
RClement5700 <Rclement5700@gmail.com>
2020-09-01 13:14:02 -0400

    Update: alter POJOs constructors

--- app/src/main/java/pojos/Order.java
+++ app/src/main/java/pojos/Order.java
@@ -1,10 +1,13 @@
 package pojos;
 
-import java.sql.Time;
 import java.util.ArrayList;
 
 public class Order {
 
     //indexed from 1 to n and described by their location in the warehouse.
-    ArrayList<Product> productList;
+    ArrayList<Product> products;
+
+    public Order(ArrayList<Product> products) {
+        this.products = products;
+    }
 }
--- app/src/main/java/pojos/Slot.java
+++ app/src/main/java/pojos/Slot.java
@@ -2,11 +2,13 @@ package pojos;
 
 public class Slot {
 
+    Product product;
     String productDescription;
     int positionId;
     int checkDigit;
 
-    public Slot(String productDescription, int positionId, int checkDigit) {
+    public Slot(Product product, String productDescription, int positionId, int checkDigit) {
+        this.product = product;
         this.productDescription = productDescription;
         this.positionId = positionId;
         this.checkDigit = checkDigit;
