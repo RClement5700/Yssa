021bcba563d21f5fe487d2310fb607682dffc6ad
RClement5700 <Rclement5700@gmail.com>
2020-09-01 13:18:08 -0400

    Update: add Product constructor

--- app/src/main/java/pojos/Product.java
+++ app/src/main/java/pojos/Product.java
@@ -1,4 +1,17 @@
 package pojos;
 
 public class Product {
+    double weight;
+    int length;
+    int width;
+    int height;
+    String description;
+
+    public Product(double weight, int length, int width, int height, String description) {
+        this.weight = weight;
+        this.length = length;
+        this.width = width;
+        this.height = height;
+        this.description = description;
+    }
 }
