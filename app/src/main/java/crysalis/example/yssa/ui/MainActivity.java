e582cb0a5e35f673dddc23cac3835fa26fbef7a0
RClement5700 <Rclement5700@gmail.com>
2020-09-03 13:35:05 -0400

    Fix: MAMP connection successful; add mysql-connector-java-5.1.45-bin.jar

--- app/src/main/java/crysalis/example/yssa/ui/MainActivity.java
+++ app/src/main/java/crysalis/example/yssa/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package crysalis.example.yssa.ui;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,6 +20,12 @@ import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import crysalis.example.yssa.R;
 import data.SectionsPagerAdapter;
@@ -55,7 +62,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        connectToSqlDatabase(getApplicationContext());
+        new ConnectToSqlDatabase().execute();
+
     }
 
     @Override
@@ -70,33 +78,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //openfire admin console http://172.20.4.51:9090
 
-    //TODO: redownload and reconfigure mamp
-    //      ask Garrett for sample URL
-    public void connectToSqlDatabase(final Context context) {
-        RequestQueue queue = Volley.newRequestQueue(context);
-        String url ="https://10.0.2.2:8889/";
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Toast.makeText(context, "MySQL oonnection successful: " + response,
-                                Toast.LENGTH_LONG).show();
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        //connect to backup cloud server
-                        Toast.makeText(context, "Connection error: " + error,
-                                Toast.LENGTH_LONG).show();
-                        System.err.println("Error: " + error);
-                    }
-                }
-        );
+    static class ConnectToSqlDatabase extends AsyncTask<String, Void, String> {
 
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        String url = "jdbc:mysql://10.0.2.2:8889/yssa";
+        String username = "root";
+        String password = "adminadmin";
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                Connection con = DriverManager.getConnection(url, username, password);
+                PreparedStatement stmt = con.prepareStatement("SELECT * FROM employees");
+                ResultSet resultSet = stmt.executeQuery();
+                if(resultSet.next()) {
+                    System.err.println(resultSet.getString(1));
+                };
+            }
+            catch (IllegalAccessException | InstantiationException | SQLException |
+                    ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
