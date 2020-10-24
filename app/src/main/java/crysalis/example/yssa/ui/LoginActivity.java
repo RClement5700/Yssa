9b1500fffa804f72728b08e8dd00db0ace6a4e5b
rclement5700 <rclement5700>
2020-10-24 04:38:04 -0700

    Fix: Authentication and SignOut working correctly

--- app/src/main/java/crysalis/example/yssa/ui/LoginActivity.java
+++ app/src/main/java/crysalis/example/yssa/ui/LoginActivity.java
@@ -4,9 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,25 +13,16 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.common.api.GoogleApi;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.concurrent.ExecutionException;
-
 import crysalis.example.yssa.R;
 import crysalis.example.yssa.databinding.ActivityLoginBinding;
 import crysalis.example.yssa.ui.managementconsole.ManagementConsoleActivity;
-import pojos.Employee;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -54,38 +44,44 @@ public class LoginActivity extends AppCompatActivity {
         loading = (ProgressBar) findViewById(R.id.loading);
         loading.setVisibility(View.GONE);
         loginBtn = (Button) findViewById(R.id.login);
-        mAuth.signOut();
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                login();
+                loading.setVisibility(View.VISIBLE);
+                String email = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                login(email, password);
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            etUsername.setText(currentUser.getEmail());
+        }
     }
 
     public void loginComplete() {
         Intent intent = new Intent(LoginActivity.this,
                 ManagementConsoleActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        System.err.println("currentuser: " +mAuth.getCurrentUser());
+        loading.setVisibility(View.GONE);
         startActivity(intent);
     }
 
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        // Check if user is signed in (non-null) and update UI accordingly.
-//        loginComplete();
-//    }
-
-    public void login() {
+    private void loginHelper(String email, String password) {
+        System.err.println("Signing in...");
         System.err.println("project id: " +
                 FirebaseApp.getInstance().getOptions().getProjectId());
         System.err.println("this project id: " +
                 mAuth.getApp().getOptions().getProjectId());
         System.err.println("API key: " + mAuth.getApp().getOptions().getApiKey());
-        mAuth.signInWithEmailAndPassword("rohan.clement@levosonus.com",
-                "yssa.clement2020")
+        mAuth.signInWithEmailAndPassword(email,
+                password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -93,7 +89,6 @@ public class LoginActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            System.err.println("Signing in...");
                             loginComplete();
                         } else {
                             // If sign in fails, display a message to the user.
@@ -101,9 +96,29 @@ public class LoginActivity extends AppCompatActivity {
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                             System.err.println(task.getException());
-//                            updateUI(null);
                         }
                     }
                 });
+        loading.setVisibility(View.GONE);
+    }
+
+    public void login(String email, String password) {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            loginComplete();
+        }
+        else {
+            if (email == null | email.isEmpty()) {
+                Toast.makeText(this, "Must enter email...", Toast.LENGTH_LONG).show();
+                loading.setVisibility(View.GONE);
+            }
+            else if (password == null | password.isEmpty()) {
+                Toast.makeText(this, "Must enter password...", Toast.LENGTH_LONG).show();
+                loading.setVisibility(View.GONE);
+            }
+            else {
+                loginHelper(email, password);
+            }
+        }
     }
 }
--- app/src/main/java/crysalis/example/yssa/ui/managementconsole/ManagementConsoleActivity.java
+++ app/src/main/java/crysalis/example/yssa/ui/managementconsole/ManagementConsoleActivity.java
@@ -1,12 +1,15 @@
 package crysalis.example.yssa.ui.managementconsole;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -17,16 +20,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import crysalis.example.yssa.R;
+import crysalis.example.yssa.ui.LoginActivity;
 
 public class ManagementConsoleActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_management_console);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        mAuth = FirebaseAuth.getInstance();
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -58,6 +64,14 @@ public class ManagementConsoleActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.management_console_menu, menu);
+        menu.getItem(1).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                mAuth.signOut();
+                startActivity(new Intent(ManagementConsoleActivity.this, LoginActivity.class));
+                return true;
+            }
+        });
         return true;
     }
 
