d6b8d3abe45880ea6ff9ea79ecdf942df66a3bfe
rclement5700 <rclement5700>
2020-10-25 16:18:56 -0700

    Issue: upon creating new user, current user is logged out and new user is logged in

--- app/src/main/java/crysalis/example/yssa/ui/managementconsole/manageassociates/ManageAssociatesFragment.java
+++ app/src/main/java/crysalis/example/yssa/ui/managementconsole/manageassociates/ManageAssociatesFragment.java
@@ -5,10 +5,12 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -42,34 +44,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 import crysalis.example.yssa.R;
+import crysalis.example.yssa.databinding.FragmentManageAssociatesBinding;
+import crysalis.example.yssa.ui.LoginActivity;
 import pojos.Employee;
 
 public class ManageAssociatesFragment extends Fragment implements View.OnClickListener {
 
+    final static String TAG = "ManageAssociates";
+    ManageAssociatesViewModel manageAssociatesViewModel;
     FirebaseAuth mAuth;
     FirebaseFirestore db;
-    final String TAG = "ManageAssociates";
-    private ManageAssociatesViewModel manageAssociatesViewModel;
-    ManageAssociatesRecyclerViewAdapter adapter;
     Context context;
+    FragmentManageAssociatesBinding binding;
     ArrayList<Employee> employees;
     RecyclerView rvManageAssociates;
-    ImageButton addUserButton;
-    ImageButton deleteUserButton;
-    ImageButton filterUserButton;
+    ManageAssociatesRecyclerViewAdapter adapter;
+    ImageButton addUserButton, deleteUserButton, filterUserButton;
+    String email, password, displayName, fullName;
+    int employeeId;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_manage_associates, container, false);
         context = getContext();
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
-        addUserButton = root.findViewById(R.id.img_btn_add_user);
+        View root = inflater.inflate(R.layout.fragment_manage_associates, container, false);
+        binding = FragmentManageAssociatesBinding.bind(root);
+        addUserButton = binding.imgBtnAddUser;
         addUserButton.setOnClickListener(this);
-        deleteUserButton = root.findViewById(R.id.img_btn_delete_user);
+        deleteUserButton = binding.imgBtnDeleteUser;
         deleteUserButton.setOnClickListener(this);
-        filterUserButton = root.findViewById(R.id.img_btn_filter_users);
+        filterUserButton = binding.imgBtnFilterUsers;
+
 //        rvManageAssociates = root.findViewById(R.id.rv_manage_associates);
 //        rvManageAssociates.setAdapter(new ManageAssociatesRecyclerViewAdapter(employees));
 //        rvManageAssociates.setLayoutManager(new LinearLayoutManager(context));
@@ -81,25 +88,7 @@ public class ManageAssociatesFragment extends Fragment implements View.OnClickLi
     public void onClick(View v) {
         switch(v.getId()) {
             case R.id.img_btn_add_user:
-                new AlertDialog.Builder(context)
-                        .setIcon(android.R.drawable.ic_input_add)
-                        .setTitle(R.string.add_user)
-                        .setView(R.layout.dialog_ui_add_user)
-                        // Specifying a listener allows you to take an action before dismissing the dialog.
-                        // The dialog is automatically dismissed when a dialog button is clicked.
-                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // add to recyclerView etc
-                                addUser("simon.mueller@levosonus.com", "password",
-                                        "simon.mueller", "Simon Mueller", 8994);
-                            }
-                        })
-
-                        // A null listener allows the button to dismiss the dialog and take no further action.
-                        .setNegativeButton(android.R.string.no, null)
-                        .show();
-
+                buildUser();
                 break;
             case R.id.img_btn_delete_user:
                 deleteUser();
@@ -107,6 +96,44 @@ public class ManageAssociatesFragment extends Fragment implements View.OnClickLi
             default:
                 break;
         }
+    }
+
+    public void buildUser() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Add User");
+        builder.setIcon(android.R.drawable.ic_input_add);
+        View addUserDialog = LayoutInflater.from(getContext()).inflate(R.layout.dialog_ui_add_user,
+                (ViewGroup) getView(), false);
+        final EditText inputEmail = addUserDialog.findViewById(R.id.et_enter_email);
+        final EditText inputPassword = addUserDialog.findViewById(R.id.et_enter_password);
+        final EditText inputDisplayName = addUserDialog.findViewById(R.id.et_enter_display_name);
+        final EditText inputFullName = addUserDialog.findViewById(R.id.et_enter_full_name);
+        final EditText inputEmployeeId = addUserDialog.findViewById(R.id.et_enter_employee_id);
+        inputEmail.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        inputPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        inputDisplayName.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+        inputFullName.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+        inputEmployeeId.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_NUMBER_VARIATION_NORMAL);
+        builder.setView(addUserDialog);
+        builder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                email = inputEmail.getText().toString();
+                password = inputPassword.getText().toString();
+                displayName = inputDisplayName.getText().toString();
+                fullName = inputFullName.getText().toString();
+                String stringEmployeeId = inputEmployeeId.getText().toString();
+                employeeId = Integer.parseInt(stringEmployeeId);
+                addUser(email, password, displayName, fullName, employeeId);
+            }
+        });
+        builder.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
 
 
@@ -144,12 +171,16 @@ public class ManageAssociatesFragment extends Fragment implements View.OnClickLi
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error writing document", e);
+                        Log.w(TAG, "Error writing document: ", e);
                     }
                 });
     }
 
     public void deleteUser() {
+
+    }
+
+    static class AddUserDialog extends Fragment {
 
     }
 }
