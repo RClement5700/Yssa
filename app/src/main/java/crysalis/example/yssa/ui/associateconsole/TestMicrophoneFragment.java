38d5d89668b5cceaaf43bfc40a2ff1864cfa00ec
rclement5700 <rclement5700>
2020-12-29 23:58:52 -0800

    TODO: fix record and playback ability in TestMicrophoneFragment

--- app/src/main/java/crysalis/example/yssa/ui/associateconsole/TestMicrophoneFragment.java
+++ app/src/main/java/crysalis/example/yssa/ui/associateconsole/TestMicrophoneFragment.java
@@ -1,7 +1,11 @@
 package crysalis.example.yssa.ui.associateconsole;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.AudioRecord;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +17,7 @@ import android.widget.Toast;
 import com.gauravk.audiovisualizer.visualizer.CircleLineVisualizer;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import java.io.IOException;
@@ -22,12 +27,18 @@ import crysalis.example.yssa.databinding.FragmentTestMicrophoneBinding;
 
 
 //https://github.com/gauravk95/audio-visualizer-android
+// AudioRecord.Builder
 
 public class TestMicrophoneFragment extends Fragment implements View.OnClickListener {
 
     ImageButton imgBtnMicrophone;
+    ImageButton imgBtnPlay;
     ImageButton imgBtnContinue;
-    AudioRecord recorder;
+    MediaRecorder recorder;
+    MediaPlayer player;
+    boolean isRecording;
+    boolean isPlaying;
+    int count;
     public TestMicrophoneFragment() {
     }
 
@@ -39,13 +50,17 @@ public class TestMicrophoneFragment extends Fragment implements View.OnClickList
         FragmentTestMicrophoneBinding binding = FragmentTestMicrophoneBinding.bind(v);
         CircleLineVisualizer visualizer = binding.testMicVizualizer;
         ProgressBar progressBar = binding.progressBar;
-        recorder = new AudioRecord.Builder()
-                .setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION)
-                .build();
+        count = 0;
+        recorder = new MediaRecorder();
+        player = new MediaPlayer();
+        isRecording = false;
+        isPlaying = false;
         imgBtnContinue = binding.imgBtnContinue;
         imgBtnMicrophone = binding.imgBtnMicrophone;
+        imgBtnPlay = binding.imgBtnPlay;
         imgBtnMicrophone.setOnClickListener(this);
         imgBtnContinue.setOnClickListener(this);
+        imgBtnPlay.setOnClickListener(this);
         return binding.getRoot();
     }
 
@@ -53,10 +68,49 @@ public class TestMicrophoneFragment extends Fragment implements View.OnClickList
     public void onClick(View v) {
         switch(v.getId()) {
             case R.id.img_btn_microphone:
-                recorder.startRecording();
+                if (!isRecording && !isPlaying) {
+                    if (ActivityCompat.checkSelfPermission(getActivity(),
+                            Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(getActivity(),
+                                new String[]{Manifest.permission.RECORD_AUDIO}, getTargetRequestCode());
+
+                        if (ActivityCompat.checkSelfPermission(getActivity(),
+                                Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+                            player.stop();
+                            isPlaying = false;
+                            recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION);
+                            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                            recorder.setOutputFile("newMediaFile");
+                            try {
+                                recorder.prepare();
+                                recorder.start();
+                                isRecording = true;
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+                else {
+                    recorder.stop();
+                    isRecording = false;
+                }
+                break;
+
+            case R.id.img_btn_play:
+                isPlaying = true;
+                isRecording = false;
+                try {
+                    player.setDataSource("newMediaFile");
+                    player.prepare();
+                    player.start();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             case R.id.img_btn_continue:
-
                 break;
         }
     }
