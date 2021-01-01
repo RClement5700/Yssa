9831a3a048561b84a3e0e1c847cee7532cadbf5c
rclement5700 <rclement5700>
2020-12-31 16:00:39 -0800

    Update: code clean up; remove fab from AssociateConsoleActivity

--- app/src/main/java/crysalis/example/yssa/ui/associateconsole/AssociateConsoleActivity.java
+++ app/src/main/java/crysalis/example/yssa/ui/associateconsole/AssociateConsoleActivity.java
@@ -1,53 +1,35 @@
 package crysalis.example.yssa.ui.associateconsole;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.auth.FirebaseAuth;
 
 import crysalis.example.yssa.R;
 import crysalis.example.yssa.databinding.ActivityAssociateConsoleBinding;
-import crysalis.example.yssa.ui.login.LoginActivity;
-import crysalis.example.yssa.ui.managementconsole.ManagementConsoleActivity;
 
 public class AssociateConsoleActivity extends AppCompatActivity implements View.OnClickListener {
-    Toolbar toolbar;
-    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
         ActivityAssociateConsoleBinding binding =
                 ActivityAssociateConsoleBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
-        FloatingActionButton fab = view.findViewById(R.id.fab_associate);
-        fab.setOnClickListener(this);
-        toolbar = view.findViewById(R.id.toolbar);
         TabLayout tabLayout = view.findViewById(R.id.associate_tab_layout);
         ViewPager viewPager = view.findViewById(R.id.associate_view_pager);
         viewPager.setAdapter(new AssociatesConsolePagerAdapter(getSupportFragmentManager(),
                 0, this));
         tabLayout.setupWithViewPager(viewPager);
-        setSupportActionBar(toolbar);
         setContentView(view);
 
     }
 
     @Override
     public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
     }
 
 //    @Override
